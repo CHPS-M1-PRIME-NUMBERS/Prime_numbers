8e185a26644fe40c206f1bdf4c38bd3d33b641b9
Damien Thenot <spaceship84@gmail.com>
2017-11-12 18:03:27 +0100

    Quelques avancées sur l'implémentation de Pocklington, on calcule nos a et b

--- pocklington.cpp
+++ pocklington.cpp
@@ -1,11 +1,12 @@
 #include "pocklington.h"
 
 int* factorisation(int n){
+        const int taille = 100000;
         if(n<2) return nullptr;
         else{
-                int *facteursObtenus = new int[10000](); int i = 0;
+                int *facteursObtenus = new int[taille](); int i = 0;
 
-                for(int j = 0; j<10000; j++) {
+                for(int j = 0; j<taille; j++) {
                         facteursObtenus[j] = 0;
                 }
 
@@ -34,6 +35,30 @@ int pgcd(int a, int b){
 bool pocklington(int candidatPrime){
         //Trouver un a, on a f un facteur de a et on a n - 1 = f * r
         //Et PGDC(f, r) = 1
+        int * facteurs = factorisation(candidatPrime-1);
+        int a = facteurs[0] * facteurs[1]; int i = 2;
+        //On récupère un a depuis les facteurs premier de candidatPrime
+        while(a*a <= candidatPrime) { //A doit être plus grand que sqrt(N) donc A² doit être plus grand que N
+                a *= facteurs[i++];
+        }
+        //On calcule b
+        int b = (candidatPrime-1)/a;
+        std::cout << "A : "<<a<<", B : "<<b<<std::endl;
+        std::cout << "A*B = "<< a*b << " "; if(a*b == candidatPrime-1) std::cout<<"True"<<std::endl; else std::cout<<"False"<<std::endl;
+        if(a*a > candidatPrime) { std::cout<<"A² > N : True"<<std::endl; } else { std::cout<<"A² > N : False"<<std::endl; }
+        std::cout << "PGCD(" << a << ", " << b << ") =  " << pgcd(a, b) << std::endl;
+
+        //Affichage de la factorisation de N-1
+        /* unsigned int i = 0;
+           if(facteurs != NULL) {
+                while(facteurs[i] != 0) {
+                        std::cout<<facteurs[i]<<" ";
+                        i++;
+                }
+                std::cout<<std::endl;
+
+                delete[](facteurs);
+           } */
 
         return false;
 }

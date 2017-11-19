f00cd3389eac72b327ae808d83d9aa5c5b50a32d
uvsq21404821 <rom1.robr@gmail.com>
2017-11-19 18:18:38 +0100

    ajout de la fonction is_perfect_power

--- AKS.cpp
+++ AKS.cpp
@@ -1,6 +1,33 @@
+#include <math.h>
+#include <iostream>
+#include <type_traits>
 
-
-bool aks(unsigned int x)
+// cette fonction vérifie que n est un "perfect power", si cette fonction renvoie true n est composé sinon aks peut continuer a s'executer
+bool is_perfect_power(unsigned int n)
+{
+	int b;
+	double a;
+	int tmp;
+	for (b=2;b <= (log(n)/log(2));b++)
+	{
+        a = pow(n,1.0/b);
+		std::cout<<"valeur de a pour b = "<< b <<" est égale a "<< a <<std::endl;
+		if((a - (int)a) == 0)
+		{
+			return true;
+		}
+	}
+	return false;
+}
+	
+	
+bool aks(unsigned int n)
 {
 	;
+}
+
+int main ()
+{
+	std::cout<<"pour n = 283 " << is_perfect_power(121) <<std::endl;
+	std::cout<<"pour n = 284 " << is_perfect_power(125) <<std::endl;
 }

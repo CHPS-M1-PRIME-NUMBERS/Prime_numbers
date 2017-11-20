0265fcf874350a48e7b9d9583377eb4bff7c00ee
Emaltard <maxence.joulin@gmail.com>
2017-11-20 09:49:05 +0100

    delete .h

--- eratosthene.h
+++
@@ -1,7 +0,0 @@
-#ifndef eratosthene_h
-#define eratosthene_h
-
-void multiple(unsigned int i, unsigned int number, bool* prime);
-bool* eratosthene(unsigned int number);
-
-#endif
--- millerRabin.h
+++
@@ -1,6 +0,0 @@
-#include <cmath>
-#include <iostream>
-
-unsigned int expPower(unsigned int x, unsigned int y, unsigned int p);
-bool millerTest(unsigned int d, unsigned int n);
-bool millerRabin(unsigned int n, unsigned int k);

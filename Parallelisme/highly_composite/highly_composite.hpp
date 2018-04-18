b341725af99e71d9f6b2d20c2a8301e24ecf7a36
Damien Thenot <spaceship84@gmail.com>
2018-04-18 17:05:48 +0200

    Ajout d'une ifndef

--- Parallelisme/highly_composite/highly_composite.hpp
+++ Parallelisme/highly_composite/highly_composite.hpp
@@ -1,3 +1,5 @@
+#ifndef highly_composite_hpp
+#define highly_composite_hpp
 #include <cmath>
 #include <cstdlib>
 #include "memory_bound.hpp"
@@ -7,3 +9,4 @@ unsigned long int nb_dividers(unsigned long int number);
 bool highly_composite_naive(unsigned long int number);
 
 bool highly_composite_def(unsigned long int number);
+#endif
